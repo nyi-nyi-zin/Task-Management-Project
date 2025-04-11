bc320579064b7482f5adbbe4077f62e5a8a603f5
Nyi Nyi Zin <nyinyizin1818@gmail.com>
2025-04-11 22:15:36 +0630

    modified 2

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
@@ -6,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   }
 );
