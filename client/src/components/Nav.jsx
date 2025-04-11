86f167ee65ea4a3bb2e5312218c74134a6365714
Nyi Nyi Zin <nyinyizin1818@gmail.com>
2025-04-12 02:12:05 +0630

    final 2

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../store/slices/userSlice";
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Nav = () => {
   const dispatch = useDispatch();
@@ -17,11 +18,7 @@ const Nav = () => {
 
   return (
     <nav className="w-full max-w-screen-xl mx-auto flex items-center justify-center text-blue-600 my-4 px-4 sm:px-6 lg:px-8">
-      <img
-        src="/a6d9eee51501460c8cb16cdebd24cad8-free.png"
-        alt="Logo"
-        className="rounded-full w-20 ml-10"
-      />
+      <img src={logo} alt="Logo" className="rounded-full w-20 ml-10" />
       {user ? (
         <section className="flex justify-end items-center gap-5 ml-auto">
           <div className="font-medium text-xl">
